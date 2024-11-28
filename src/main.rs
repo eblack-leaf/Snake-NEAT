8f3e611dd200db546cc8e2461710dd307f8792c4
salt <eblack.leaf@proton.me>
2024-11-27 20:11:17 -0500

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+use foliage::Foliage;
 
+const VIEW_AREA: (f32, f32) = (1600.0, 800.0);
+fn main() {
+    let mut foliage = Foliage::new();
+    foliage.set_desktop_size(VIEW_AREA);
+    foliage.photosynthesize();
 }
